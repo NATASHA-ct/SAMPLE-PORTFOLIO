6b9b250608a0b1070cfbe3352ddaf8d86ec147c9
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-06-21 01:52:02 +0300

    Fix linter error

--- javascript/myportfolio.js
+++ javascript/myportfolio.js
@@ -1,33 +1,24 @@
-const menu = document.querySelector(".menu");
-console.log(menu);
+const menu = document.querySelector('.menu');
 
-const navlist = document.querySelector(".nav-list");
-
+const navlist = document.querySelector('.nav-list');
 
 // Adding click event to the hamburger
 
-menu.addEventListener("click",() => {
+menu.addEventListener('click', () => {
+  menu.classList.toggle('active');
 
-    menu.classList.toggle("active");
-
-    navlist.classList.toggle("active");
-}
-);
-
-  document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",() => {
-
-    menu.classList.remove("active");
-
-    navlist.classList.remove("active");
-    
-}));
-
-//Adding event to close icon on menu
-document.querySelector(".close-icon").addEventListener("click", () => {
- 
-      menu.classList.remove("active");
-
-      navlist.classList.remove("active");
-
+  navlist.classList.toggle('active');
 });
 
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  menu.classList.remove('active');
+
+  navlist.classList.remove('active');
+}));
+
+// Adding event to close icon on menu
+document.querySelector('.close-icon').addEventListener('click', () => {
+  menu.classList.remove('active');
+
+  navlist.classList.remove('active');
+});
